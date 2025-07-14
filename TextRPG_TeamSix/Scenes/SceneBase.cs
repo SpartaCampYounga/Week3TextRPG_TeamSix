2b56145e68f8bdfa319d5898c25b8969ccb29c61
Younga <youngajin93@gmail.com>
2025-07-14 11:23:07 +0900

    InputHelper

--- TextRPG_TeamSix/Scenes/SceneBase.cs
+++ TextRPG_TeamSix/Scenes/SceneBase.cs
@@ -19,27 +19,5 @@ namespace TextRPG_TeamSix.Scenes
 
         public abstract void HandleInput(); //입력 받고 실행하는 시스템
 
-        protected int GetIntegerRange(int min, int max) //min ~ (max-1) 사이의 입력을 받음.
-        {
-            bool isSuccessful = false;
-            int integer;
-
-            do
-            {
-                string input = Console.ReadLine();
-                isSuccessful = int.TryParse(input, out integer) && integer >= min && integer < max;
-
-                if (!isSuccessful)
-                {
-                    Console.Write($"{min}~{max - 1} 범위의 숫자만 입력해주세요.\n>>");
-                }
-            } while (!isSuccessful);
-            return integer;
-        }
-        protected void WaitResponse()
-        {
-            Console.WriteLine("0. 다음");
-            int input = GetIntegerRange(0, 1);
-        }
     }
 }
