6287ce6a34af45e342840cfd5d581ec7c4a775fa
장보석 (Jang Bo Seok) <heeno7700@gmail.com>
2025-07-15 10:44:24 +0900

    Update Inventory.cs
    구매 구현

--- TextRPG_TeamSix/Character/Inventory.cs
+++ TextRPG_TeamSix/Character/Inventory.cs
@@ -2,29 +2,75 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using TextRPG_TeamSix.Interfaces;
+using System.Threading.Tasks;
 using TextRPG_TeamSix.Items;
+using System.Security.Cryptography.X509Certificates;
 
-namespace TextRPG_TeamSix.Character
+namespace TextRPG_TeamSix.Characters
 {
     //인벤토리 관리
     internal class Inventory //: IContainableItems
     {
-        public List<Item> InventoryContainer { get; private set; } = new List<Item>();
+        public Player Owner { get; private set; }
+        public List<Item> ItemList { get; private set; } = new List<Item>();
 
-        public Item? GetItem(uint id)
+        public Item(uint id, string name, string description, uint price)
         {
-            Item item = InventoryContainer.FirstOrDefault(x => x.Id == id);
-            return item;
-
+            Id = id;
+            Name = name;
+            Description = description;
+            Price = price;
         }
+        public Inventory(Player owner)
+        {
+            Owner = owner;
+        }
+
         public void DisplayItems()
         {
             //리스트 내부 아이템 전부 출력
+            if(ItemList.Count == 0)
+            {
+            Console.WriteLine("인벤토리가 비어 있습니다.");
+                return;
+            }
+            Console.WriteLine("인벤토리");
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine(" ID | 이름 | 능력 | 설명 |");
+
+            for(int i = 0; i <ItemList.Count; i++)
+            {
+                var item = ItemList[i];
+                Console.WriteLine($"{item.Id} | {item.Name} | {item.Description} | {item.Price}");
+            } 
         }
-        public void PurchaseItem(uint itemId)
+        public Item? GetItem(uint id)
         {
+            Item? item = ItemList.FirstOrDefault(x => x.Id == id);
+            return item;
+
+        }
+        public void PurchaseItem(uint itemId) 
+        {
+            Item? item = GetItem(itemId);
+            if (item == null)
+            {
+                Console.WriteLine("해당 아이템이 존재하지 않습니다.");
+                return;
+            }
+            // 구매 로직 추가
+            // 예: 플레이어의 골드가 충분한지 확인하고, 아이템을 인벤토리에 추가
+            if (Owner.Gold >= item.Price)
+            {
+                Owner.Gold -= item.Price;
+                ItemList.Add(item);
+                Console.WriteLine($"{item.Name}을(를) 구매했습니다.");
+            }
+            else
+            {
+                Console.WriteLine("골드가 부족합니다.");
+            }
 
         }
         public void SellItem(uint itemId)
