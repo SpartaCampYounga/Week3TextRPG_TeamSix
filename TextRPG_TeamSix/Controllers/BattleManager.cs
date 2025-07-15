07186a214ee9e9b655d91c20b1bebd8ff6c6d4ce
Winebug <ydjoo0410@gmail.com>
2025-07-15 09:36:18 +0900

    Update BattleManager.cs
    Branch Test

--- TextRPG_TeamSix/Controllers/BattleManager.cs
+++ TextRPG_TeamSix/Controllers/BattleManager.cs
@@ -4,9 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TextRPG_TeamSix.Controllers
+namespace TextRPG_TeamSix.Game
 {
     internal class BattleManager
     {
+
+        public void StartBattle()
+        {
+           
+        }
+
+        public void ExecuteTurn()
+        {
+            
+        }
+        public void BattleOver()
+        {
+
+        }
+
+        public void EndBattle()
+        {
+
+        }
     }
 }
