e9d9a8a7392f82421cc50289525d0ef024a9c69e
SeungIl <138782327+NekrosGame@users.noreply.github.com>
2025-07-15 15:59:45 +0900

    (update) Quest

---
+++ TextRPG_TeamSix/Enums/QuestType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG_TeamSix.Enums
+{
+    internal enum QuestType
+    {
+        // 퀘스트 타입을 정의. (몬스터, 던전)
+    }
+    
+}
---
+++ TextRPG_TeamSix/Quest/Quest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextRPG_TeamSix.Characters;
+using TextRPG_TeamSix.Enums;
+
+namespace TextRPG_TeamSix.Quest
+{
+    internal abstract class Quest
+    {
+        public QuestType QuestType { get; }
+        public string Description { get; protected set; }
+        public uint RewardGold { get; private set; }
+        public uint RewardExp { get; private set; }
+        //public void Reward(Player player);
+
+        public Quest(QuestType questType, string description, uint rewardGold, uint rewardExp)
+        {
+            QuestType = questType;
+            Description = description;
+            RewardGold = rewardGold;
+            RewardExp = rewardExp;
+        }
+    }
+}
