b8a1568ca5622ac8a86ad1558fe1a67fe1c4c5ae
NakHyeong <snh0719@gmail.com>
2025-07-15 09:44:32 +0900

    오타발견

--- TextRPG_TeamSix/Stores/Store.cs
+++ TextRPG_TeamSix/Stores/Store.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace TextRPG_TeamSix.Stores
-{
     using global::TextRPG_TeamSix.Items;
     using System;
     using System.Collections.Generic;
