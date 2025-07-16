e43b3d40b2fe03edfde7b3afbb305ca17dfbd07d
Winebug <ydjoo0410@gmail.com>
2025-07-16 14:14:34 +0900

    .
    .

--- TextRPG_TeamSix/Program.cs
+++ TextRPG_TeamSix/Program.cs
@@ -10,9 +10,11 @@ namespace TextRPG_TeamSix
         static void Main(string[] args)
         {
             GameInitializer.InitializeAll();
-            SceneManager.Instance.SetScene(SceneType.Store);
+            //SceneManager.Instance.SetScene(SceneType.Store);
 
             //SceneManager.Instance.SetScene(SceneType.Title);
+
+            SceneManager.Instance.SetScene(SceneType.Battle);
         }
     }
 }
