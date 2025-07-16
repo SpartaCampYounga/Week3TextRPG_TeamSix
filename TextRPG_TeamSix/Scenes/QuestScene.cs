b0c2b148d56b9caf828e51e2f77ad0f8a14baa99
SeungIl <138782327+NekrosGame@users.noreply.github.com>
2025-07-16 14:04:23 +0900

    중간저장

--- TextRPG_TeamSix/Scenes/QuestScene.cs
+++ TextRPG_TeamSix/Scenes/QuestScene.cs
@@ -27,32 +27,32 @@ namespace TextRPG_TeamSix.Scenes
             Console.WriteLine(new string('=', 120));
 
 
-            // Hard던전 클리어  | Hard던전을 소탕하세요. | 보상: 1000골드, 100경험치
+            // 들어갈 내용 예시) Hard던전 클리어  | Hard던전에 모든 적들을 소탕해주세요! | 보상: 1000골드, 100경험치
             string header = "";
             header += FormatUtility.AlignWithPadding("퀘스트 목표", 15) + " | ";
             header += FormatUtility.AlignWithPadding("내용", 30) + " | ";
             header += FormatUtility.AlignWithPadding("보상", 30) + " | ";
+            header += FormatUtility.AlignWithPadding("진행사항", 0);
 
             Console.WriteLine(header);
             Console.WriteLine(new string('-', 120));
-
-
             Console.ResetColor();
 
+            // 퀘스트 수락시 퀘스트 리스트를 번호로 선택가능하게 해야함
+            // 선택한 퀘스트는 진행중으로 표기, 완료시 다시 선택가능하게 해야함
 
             Console.WriteLine("퀘스트 배치구역");
 
 
+            // 퀘스트 끝나는 부분
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine(new string('=', 120));
             Console.ResetColor();
 
 
 
-
+            Console.WriteLine("1. 퀘스트 수락(미구현)");
             Console.WriteLine("0. 나가기");
-
-
             Console.WriteLine("번호를 입력해 주세요 : ");
             input = InputHelper.GetIntegerRange(0, 1);
             HandleInput();
