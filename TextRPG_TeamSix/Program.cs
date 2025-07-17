5615050a13c3e89342989d6a615f7cbd69ace892
장보석 (Jang Bo Seok) <heeno7700@gmail.com>
2025-07-17 12:50:31 +0900

    아이템 추가 및 상태창 디자인 추가

--- TextRPG_TeamSix/Program.cs
+++ TextRPG_TeamSix/Program.cs
@@ -13,8 +13,8 @@ namespace TextRPG_TeamSix
             GameInitializer.InitializeAll();
 
             //SceneManager.Instance.SetScene(SceneType.Title);
-           // SceneManager.Instance.SetScene(SceneType.Title);
-            SceneManager.Instance.SetScene(SceneType.Battle);
+           SceneManager.Instance.SetScene(SceneType.Title);
+            //SceneManager.Instance.SetScene(SceneType.Battle);
 
         }
     }
