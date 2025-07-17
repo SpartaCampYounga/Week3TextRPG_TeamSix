02746668d95aed76e404d67e2e63ddec480e7c55
NakHyeong <snh0719@gmail.com>
2025-07-17 18:48:08 +0900

    flx
    .

---
+++ TextRPG_TeamSix/Enums/StoreMode.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG_TeamSix.Enums
+{
+    internal enum StoreMode
+    {
+        None, //초기값
+        Buy,  //구매모드
+        Sell  //판매모드
+    }
+}
