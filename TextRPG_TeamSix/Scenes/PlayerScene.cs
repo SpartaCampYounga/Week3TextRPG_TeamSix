eae04dfe6cc726e6b0e885e1931752b692650a3a
Younga <youngajin93@gmail.com>
2025-07-17 10:43:24 +0900

    [feat]게임 진행 플로우 개선
    임시 개발용으로 붙여놓은 부분들을 플로우차트 대로 개선함

--- TextRPG_TeamSix/Scenes/PlayerScene.cs
+++ TextRPG_TeamSix/Scenes/PlayerScene.cs
@@ -10,6 +10,7 @@ using TextRPG_TeamSix.Items;
 using TextRPG_TeamSix.Stores;
 using TextRPG_TeamSix.Quests;
 using TextRPG_TeamSix.Utilities;
+using TextRPG_TeamSix.Game;
 
 namespace TextRPG_TeamSix.Scenes
 {
@@ -23,7 +24,9 @@ namespace TextRPG_TeamSix.Scenes
         private List<Item> items = new List<Item>();
         private List<Quest> Quests = new List<Quest>();
 
-        private MainScene mainScene = new MainScene(); // MainScene 인스턴스 생성
+        int input;
+
+        //private MainScene mainScene = new MainScene(); // MainScene 인스턴스 생성
 
         public override void DisplayScene()
         {
@@ -40,43 +43,67 @@ namespace TextRPG_TeamSix.Scenes
             Console.WriteLine($"Exp : {player.Exp} / 100");
             Console.WriteLine($"돌의 개수 : {player.NumOfStones}");
             Console.WriteLine("-------------------------------------");
-            Console.WriteLine("1. 인벤토리 확인");
-            Console.WriteLine("2. 받은 퀘스트 확인하기");
+            Console.WriteLine("1. 인벤토리");
+            Console.WriteLine("2. 스킬");
             Console.WriteLine("0. 나가기");
-            while (true)
-            {
-                Console.Write("원하는 번호를 입력하세요: ");
-                int userInpt;
-                bool InputChoice = int.TryParse(Console.ReadLine(), out userInpt);
-                if (!InputChoice)
-                {
-                    Console.WriteLine("잘못된 입력입니다. 다시 시도해주세요.");
-                    return;
-                }
-                switch (userInpt)
-                {
-                    case 1:
-                        player.Inventory.DisplayItems(); // 인벤토리 출력
-                        break;
-                    case 2:
 
-                        break;
-                    case 0:
-                        Console.WriteLine("상태창을 나갑니다.");
-                        mainScene.DisplayScene(); // MainScene 인스턴스를 사용하여 호출
-                        break;
-                    default:
-                        Console.WriteLine("잘못된 입력입니다. 다시 시도해주세요.");
-                        break;
-                }
-                DisplayScene(); //다시 출력
-            }
+
+            Console.Write("번호를 입력하세요 :");
+
+            input = InputHelper.GetIntegerRange(0, 3);
+            HandleInput();
+            //while (true)
+            //{
+            //    Console.Write("원하는 번호를 입력하세요: ");
+            //    int userInpt;
+            //    bool InputChoice = int.TryParse(Console.ReadLine(), out userInpt);
+            //    if (!InputChoice)
+            //    {
+            //        Console.WriteLine("잘못된 입력입니다. 다시 시도해주세요.");
+            //        return;
+            //    }
+            //    switch (userInpt)
+            //    {
+            //        case 1:
+            //            SceneManager.Instance.SetScene(SceneType.Inventory); //씬 호출하는 방식으로 변경
+            //            //player.Inventory.DisplayItems(); // 인벤토리 출력
+            //            break;
+            //        case 2:
+            //            SceneManager.Instance.SetScene(SceneType.Skill);
+            //            break;
+            //        case 0:
+            //            Console.WriteLine("상태창을 나갑니다.");
+            //            SceneManager.Instance.SetScene(SceneType.Main);
+            //            //mainScene.DisplayScene(); // MainScene 인스턴스를 사용하여 호출
+            //            break;
+            //        default:
+            //            Console.WriteLine("잘못된 입력입니다. 다시 시도해주세요.");
+            //            break;
+            //    }
+            //    //DisplayScene(); //다시 출력  //이렇게 진행할 경우, 메인 화면에서 나가기를 선택하였을 때 종료가 되지 않고 캐릭터 선택 창으로 오게 됩니다. 비교를 위해서 while문 놔두고 주석 처리 하였습니다.
+            //}
 
         }    //출력 하는 시스템
 
         public override void HandleInput()
         {
-
+            switch (input)
+            {
+                case 1:
+                    Console.WriteLine("1선택함");
+                    Console.Read();
+                    SceneManager.Instance.SetScene(SceneType.Inventory); //씬 호출하는 방식으로 변경
+                    //player.Inventory.DisplayItems(); // 인벤토리 출력
+                    break;
+                case 2:
+                    SceneManager.Instance.SetScene(SceneType.Skill);
+                    break;
+                case 0:
+                    Console.WriteLine("상태창을 나갑니다.");
+                    SceneManager.Instance.SetScene(SceneType.Main);
+                    //mainScene.DisplayScene(); // MainScene 인스턴스를 사용하여 호출
+                    break;
+            }
         } //입력 받고 실행하는 시스템
 
     }
